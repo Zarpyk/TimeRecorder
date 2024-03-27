43d8bf05345aefcaaf61f67a1bc5b45d0aa0a6ea
Zarpyk <30746531+Zarpyk@users.noreply.github.com>
2024-03-27 20:14:34 +0100

    Add project and tag properties

--- TimeRecorderDomain/Models/Tag.cs
+++ TimeRecorderDomain/Models/Tag.cs
@@ -4,5 +4,7 @@ namespace TimeRecorderDomain.Models {
     public class Tag : IDBObject {
         [BsonId]
         public string ID { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; } = string.Empty;
+        public string Color { get; set; } = "#FFFFFF";
     }
 }
