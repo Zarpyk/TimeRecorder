458d5bd8dbdac1790129cd3fe5e89c32c616d0ef
Zarpyk <30746531+Zarpyk@users.noreply.github.com>
2024-03-28 19:36:57 +0100

    Add project and tag DTO

---
+++ TimeRecorderAPI/Src/DTO/ProjectDTO.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using TimeRecorderDomain.Models;
+
+namespace TimeRecorderAPI.DTO {
+    public record ProjectDTO {
+        public Guid? ID { get; internal set; }
+        public string? Name { get; set; }
+        public string? Color { get; set; }
+    }
+
+    public class ProjectDTOValidator : AbstractValidator<ProjectDTO> {
+        public ProjectDTOValidator() {
+            RuleFor(x => x.Color).Matches("^#([A-Fa-f0-9]{6})$");
+        }
+    }
+}
---
+++ TimeRecorderAPI/Src/DTO/TagDTO.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace TimeRecorderAPI.DTO {
+    public record TagDTO {
+        public Guid? ID { get; internal set; }
+        public string? Name { get; set; }
+        public string? Color { get; set; }
+    }
+
+    public class TagDTOValidator : AbstractValidator<TagDTO> {
+        public TagDTOValidator() {
+            RuleFor(x => x.Color).Matches("^#([A-Fa-f0-9]{6})$");
+        }
+    }
+}
